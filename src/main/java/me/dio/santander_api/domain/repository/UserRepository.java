fa9f6d540df5128e1869634986ebfad62760ff80
“jeferson” <jefersonsatoshi@hotmail.com>
2025-04-08 02:07:25 -0300

    Implementação da camada de dominio

---
+++ src/main/java/me/dio/santander_api/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.dio.santander_api.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.santander_api.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
